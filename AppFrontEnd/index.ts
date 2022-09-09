719d718c844f824b94f9c1e5a1e9941bc49b609c
FunctionOne <leonardozivieri150@gmail.com>
2022-09-09 10:34:40 -0300

    New AppFrontEnd function

---
+++ AppFrontEnd/index.ts
@@ -0,0 +1,26 @@
+﻿import * as df from "durable-functions";
+import {
+  AzureFunction,
+  Context,
+  HttpRequest,
+  HttpResponseSimple,
+} from "@azure/functions";
+import { readFile } from "fs/promises";
+import { join } from "path";
+
+let html = readFile(join(__dirname, "..", "..", "index.html")).then((b) =>
+  b.toString()
+);
+const httpStart: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<any> {
+  return <HttpResponseSimple>{
+    body: await html,
+    headers: {
+        "Content-Type": "text/html"
+    }
+  };
+};
+
+export default httpStart;
